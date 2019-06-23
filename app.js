02f62e80d2e24c75b9ea13d81f8eb12f5edab79e
Christian <cymourad@uwaterloo.ca>
2019-06-22 21:52:49 -0400

    Level 2 - Added env keys

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -23,9 +24,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "IlikeWatosmorethanAdam.";
 userSchema.plugin(encrypt, {
-  secret: secret,
+  secret: process.env.SECRET,
   encryptedFields: ['password']
 });
 
