b619ec5a6c27e602a7cf30e6cfb131cc6d9f1f36
Christian <cymourad@uwaterloo.ca>
2019-06-24 08:21:56 -0400

    Level 3 - md5 Hash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -24,11 +24,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/home", function(req, res) {
@@ -46,7 +41,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -70,7 +65,7 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
+        if (foundUser.password === md5(password)) {
           res.render("secrets");
         }
       }
